2d6e2480be8671c1fb6d7078e342ef884e700519
Avi Biton <abiton@redhat.com>
2025-01-12 23:19:48 +0200

    fix lint issues
    Signed-off-by: Avi Biton <abiton@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -66,7 +66,7 @@ func main() {
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.BoolVar(&secureMetrics, "metrics-secure", true,
-		"If set the metrics endpoint is served securely")
+		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	opts := zap.Options{
